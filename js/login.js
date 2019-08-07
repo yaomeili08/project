9f7fad5ccd461afe6e3f73f04936bb2fd3781d9d
yml <ymlyaoml@163.com>
2019-08-07 22:54:41 +0800

    完成登录页面

--- js/login.js
+++ js/login.js
@@ -1,24 +1,64 @@
 $(function(){
-	 $(".logbox .submit").click(function(){
-	 	//获取数据
-	 	var user = $(".logbox .user").val();
-	 	var psw =$(".logbox .pwd").val();
-	 	 //console.log(user,psw);
-	 	//发送请求
-	 	$.ajax({
+	   
+	   	 //发送请求封装
+	   function sendLogin(str){	 
+	 	 $.ajax({
 	 	   type:"post",
 	 	   url:"../php/login.php",
-	 	   data:`username=${user}&password=${psw}`,
-	 	   datatype:"json",
-	 	   success:function(response){
+	 	   data: str,
+	 	   dataType:"json",
+	 	   success:function(response){ 
 	 	     if(response.status == "success"){
-            		alert(response.msg);
+	 	     	  console.log(response.msg,"00");
+            		alert(response.msg); 
             	   window.location.href = "../indexgome.html";
-               }else{
+               }else{ 
             		alert(response.msg);
+            		//清空当前页面cookie数据
+            		 Cookie.clear();
             	 }
-	 	   }
-	 	})
+ 	 	     }
+          })
+         }
+	    
+	 //检查当前页面中是否存在cookie值
+	 let queryAuto = "";
+	 let tex1 =  Cookie.hasItem("username");
+	 let tex2 =  Cookie.hasItem("password");
+	 console.log(tex1,tex2);
+	 if(tex1 && tex2){	 	    
+	 	 let userName = Cookie.getItem("username");
+	 	 let  pass  = Cookie.getItem("password"); 
+	 	console.log(userName,pass);
+	 	  queryAuto = `username=${userName}&password=${pass}`;
+	 	  sendLogin(queryAuto); 
+	   }
+	     //输入密码登陆
+	 else{
+	  
+	  $(".logbox .submit").click(function(){
+	 	//获取数据
+	 	var user = $.trim($(".logbox .user").val());
+	 	var psw =$.trim($(".logbox .pwd").val());
+	 	var auto = $("#autoLog").is(":checked");
 	 	
-	 })
+	 	//非空判断
+	 	 if(user.length == 0  || psw == 0 ){
+	 	 	  alert("用户名以及密码不能为空！"); 
+	 	}
+	 	else{ 
+	 	  //判断自动登陆按钮是否选中 将用户名及密码存入cookie
+	 	  if(auto){
+	 	 	  Cookie.setItem("username",user);
+	 	 	  Cookie.setItem("password",psw); 	 	   
+	 	  }
+	 	 queryAuto = `username=${user}&password=${psw}`;
+	 	 	//发送请求
+	 	  sendLogin(queryAuto);	
+	 	 
+	 	 
+	 	}
+	  })
+	  
+	} 
 })
--- php/login.php
+++ php/login.php
@@ -1,38 +1,38 @@
-<?php
-  $username = isset($_POST["username"])? $_POST["username"] : "" ;
-  $password = isset($_POST["password"])? $_POST["password"] : "" ;
-  
-  //连接数据库并进行查找
-  $com = mysqli_connect("127.0.0.1","root","","guomei");
-  
-  //通过查找用户名
-  $sql = "SELECT * FROM  `resgister` WHERE  `uesrname`  LIKE '$username'";
-  //通过查找手机号
-//$sql2 = "SELECT * FROM  `resgister` WHERE  `phone`  LIKE '$uesrname'  ";
+<?php  header("Content-Type:text/html; charset=utf8");
+	
+// $uesrname = isset($_POST["username"])? $_POST["username"] : "" ;
+// $password = isset($_POST["password"])? $_POST["password"] : "" ;
    
-  $data = array("status" => "", "msg" => "", "data" => "");
-     
-  //执行命令
-  $result = mysqli_query($com,$sql);
-   //数据是否存在
-  if(mysqli_num_rows($result) == "0"){
-  	 $data["status"] = "error";
+   // username=56566&password=jjhjjk
+   $usernameA = $_REQUEST["username"];
+   $passwordA = $_REQUEST["password"];
+// echo $usernameA;
+// echo $passwordA;
+ 
+//连接数据库并进行查找
+$com = mysqli_connect("127.0.0.1","root","","guomei");
+
+//通过查找用户名
+$sql = "SELECT * FROM  `resgister` WHERE  `uesrname`  LIKE '$usernameA'";
+//执行命令
+$result = mysqli_query($com,$sql); 
+//
+$data = array("status"=>"", "msg"=>"", "data"=>"");
+$psw = mysqli_fetch_array($result);
+ //数据是否存在
+if(mysqli_num_rows($result) == "0"){
+	 $data["status"] = "error";
      $data["msg"] = "登录失败：该用户不存在";
+}else{
+   /*密码是否正确 */
+  if($psw["password"] != $passwordA)
+  {
+    $data["status"] = "error";
+    $data["msg"] = "密码不正确！";
+  }else{
+    $data["status"] = "success";
+    $data["msg"] = "恭喜你，登录成功！";
   }
-  echo $data["msg"];
-  //echo json_echode($data,true);
-//else
-//{
-  	/* 检查密码是否正确 */
-//if( mysqli_fetch_array($result)["password"] != $password)
-// {
-//  $data["status"] = "error";
-//  $data["msg"] = "登录失败：密码不正确！";
-//}
-//else{
-//  $data["status"] = "success";
-//  $data["msg"] = "登录成功！";
-// }
-//}
-//echo json_echode($data,true);
+}
+echo json_encode($data,true);
 ?>
